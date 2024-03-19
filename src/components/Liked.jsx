3732bd119cf35442143c25afac2be68b981f27a3
MaciejBascik <96304157+MaciejBascik@users.noreply.github.com>
2024-03-19 22:23:41 +0100

    functionality changes

--- src/components/Liked.jsx
+++ src/components/Liked.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LikedFeedBar,LikedCard, Loader } from './index.js';
+import { LikedFeedBar,LikedCard } from './index.js';
 import { useState } from 'react';
 import {Stack} from '@mui/material';
 import NoTracksAlert from './Alert.jsx';
@@ -9,14 +9,8 @@ const Liked = () => {
   const location = useLocation();
   const data = location.state;
 
-  const [receivedData, setReceivedData] = useState('');
-  const handleChildData = (dataFromChild) => {
-    setReceivedData(dataFromChild);
-  };
-  
   if(!data.length) return (
     <>
-
     <Stack sx={{ flexDirection: 'column', background:'#ecfdf4', minHeight:'90vh', alignItems:'center'}} >
     <LikedFeedBar/>
   <NoTracksAlert text="Brak polubionych utworów!"/> 
@@ -28,7 +22,7 @@ const Liked = () => {
 <Stack sx={{ flexDirection: 'column', background:'#ecfdf4', minHeight:'90vh'}} >
     <LikedFeedBar/>
     <Stack direction="row" flexWrap="wrap" justifyContent="center" gap={2} >
-      {(data.length == 0) && ( <NoTracksAlert text="Brak polubionych utworów!"/> )}
+      {(data.length === 0) && ( <NoTracksAlert text="Brak polubionych utworów!"/> )}
         {
           data.map((item,idx) => (
             <LikedCard item={item} idx={idx}/>
